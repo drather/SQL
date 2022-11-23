5268218ca79787a64fb8704de3a88c932951fa62
kks <drather@naver.com>
2022-11-24 00:16:25 +0900

    solved: Leetcode 180 consecutive numbers - add answer query that is same with my answer.

--- LeetCode_184_Department_Highest_Salary_2.sql
+++ LeetCode_184_Department_Highest_Salary_2.sql
@@ -14,3 +14,22 @@ FROM (
     ON EMPLOYEE.departmentId = DEPARTMENT.id
 ) res
 WHERE res.salary = res.max_salary
+
+
+-- answer query
+-- it's the same with above SQL
+
+--SELECT MAXIMUM_SALARY.Department,
+--        MAXIMUM_SALARY.name AS Employee,
+--        MAXIMUM_SALARY.salary
+--FROM (
+--    SELECT EMPLOYEE.name,
+--            EMPLOYEE.salary,
+--            DEPARTMENT.name AS Department,
+--            MAX(salary) OVER (PARTITION BY departmentId) AS max_salary
+--    FROM Employee
+--        INNER JOIN DEPARTMENT
+--        ON EMPLOYEE.departmentId = DEPARTMENT.id
+--) MAXIMUM_SALARY
+--
+--WHERE MAXIMUM_SALARY.salary = MAXIMUM_SALARY.max_salary

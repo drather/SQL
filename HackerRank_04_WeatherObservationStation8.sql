13a5d8bc12002a9ae4327ce89e038378f8c9cc65
kks <drather@naver.com>
2022-11-27 20:12:06 +0900

    solved: HackerRank WeatherObservationStation7 with Regular Expression, start with, end with combination

---
+++ HackerRank_04_WeatherObservationStation8.sql
@@ -0,0 +1,13 @@
+-- 정규표현식 풀이 문제
+-- 이전 2가지 조건을 combination 하는 문제이다.
+-- ^[aeiou] : 모음으로 시작한다.
+-- .*: 아무것이나 와도 상관이 없다.
+-- [aeiou]$: 모음으로 끝난다.
+
+-- 정규표현식 튜토리얼 사이트:
+-- https://regexone.com/lesson/introduction_abcs
+-- https://regexr.com/
+
+SELECT DISTINCT city
+FROM STATION
+WHERE city REGEXP '^[aeiou].*[aeiou]$'
